520ce09e5c77652499ec81385eca9e6a42387e84
Michal MIchaluk <michal.michaluk@bottega.com.pl>
2024-12-17 05:25:30 +0100

    exercise 02 end

--- src/main/java/devices/configuration/intervals/IntervalRulesDocumentRepository.java
+++ src/main/java/devices/configuration/intervals/IntervalRulesDocumentRepository.java
@@ -1,20 +1,24 @@
 package devices.configuration.intervals;
 
+import devices.configuration.tools.FeatureConfiguration;
 import lombok.AllArgsConstructor;
-import org.apache.commons.lang3.NotImplementedException;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
 class IntervalRulesDocumentRepository implements IntervalRulesRepository {
 
-    @Override
-    public IntervalRules get() {
-        throw new NotImplementedException("Not implemented yet");
-    }
+    public static final String CONFIG_NAME = "IntervalRules";
+    private final FeatureConfiguration repository;
 
     @Override
+    public IntervalRules get() {
+        return repository.get(CONFIG_NAME)
+                .as(IntervalRules.class)
+                .orElse(IntervalRules.defaultRules());
+    }
+
     public void save(IntervalRules configuration) {
-        throw new NotImplementedException("Not implemented yet");
+        repository.save(CONFIG_NAME, configuration);
     }
 }
