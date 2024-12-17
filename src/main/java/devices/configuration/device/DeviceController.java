de41849827fc932678d8e21eaab3de4a7d9b8217
Michal MIchaluk <michal.michaluk@bottega.com.pl>
2024-12-17 05:57:51 +0100

    exercise 05 end

---
+++ src/main/java/devices/configuration/device/DeviceController.java
@@ -0,0 +1,28 @@
+package devices.configuration.device;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@RestController
+@RequiredArgsConstructor
+class DeviceController {
+
+    private final DeviceService service;
+
+    @PatchMapping(path = "/devices/{deviceId}",
+            consumes = APPLICATION_JSON_VALUE,
+            produces = APPLICATION_JSON_VALUE)
+    DeviceConfiguration patchStation(@PathVariable String deviceId,
+                                     @RequestBody @Valid UpdateDevice update) {
+        return service.updateDevice(deviceId, update)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+}
